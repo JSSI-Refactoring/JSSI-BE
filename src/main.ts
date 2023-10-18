8f76e71888a07ebc9343bf44b3d123c4bc0f0496
waveinyu <waveinyu@gmail.com>
2023-10-18 14:09:31 +0900

    Chore: set global prefix

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const port = 3001;
+  const port = 3000;
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api', { exclude: ['health'] });
   console.log(`DB connection`);
 
   await app.listen(port);
