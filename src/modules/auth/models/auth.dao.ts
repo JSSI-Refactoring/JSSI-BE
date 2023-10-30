0619eec4f5896a055e647fc79022aed8a3467f33
waveinyu <waveinyu@gmail.com>
2023-10-30 17:01:52 +0900

    Fix: `auth.dao.ts` - `kakaoSignUp` 분기 처리 수정

--- src/modules/auth/models/auth.dao.ts
+++ src/modules/auth/models/auth.dao.ts
@@ -14,19 +14,25 @@ export class AuthDAO {
     const { nickname } = userInfo.properties;
     const profileImage = userInfo.properties.profile_image;
 
-    // 2-5. 신규 회원일 경우 회원가입
+    // 2-5. 신규 회원 유무 판단
     const checkThisUser = await this.userRepository.findOne({ where: { socialId } });
-    if (!checkThisUser) {
-      await this.userRepository.save({ socialId, socialType: 'KAKAO', nickname, profileImage });
-    }
+    const isNewUser = !checkThisUser; // checkThisUser가 없으면 true, 있으면 false
+    console.log(`이 유저는 새로운 유저인가요: ${isNewUser}`);
 
-    const payload = {
-      id: checkThisUser.id,
-      nickname: checkThisUser.nickname,
-      profileImage: checkThisUser.profileImage,
+    // 2-6. 페이로드 값 세팅(추가)
+    const userPayload = {
+      id: isNewUser ? null : checkThisUser.id,
+      nickname: isNewUser ? nickname : checkThisUser.nickname,
+      profileImage: isNewUser ? profileImage : checkThisUser.profileImage,
     };
 
-    // 2-6. 기존 회원일 경우 서버에서 로그인 세션 또는 토큰 발급
-    return { token: await this.jwtService.signAsync(payload) };
+    // 2-7. 신규 회원일 경우 create(추가)
+    if (isNewUser) {
+      const createUser = await this.userRepository.save({ socialId, socialType: 'KAKAO', ...userPayload });
+      userPayload.id = createUser.id;
+    }
+
+    // 2-8. 기존 회원일 경우 서버에서 로그인 세션 또는 토큰 발급
+    return { token: await this.jwtService.signAsync(userPayload) };
   }
 }
