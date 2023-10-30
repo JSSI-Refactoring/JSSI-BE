b259d238e7a7c338d10d39cb388250cbbfe84dd0
waveinyu <waveinyu@gmail.com>
2023-10-30 17:58:55 +0900

    Fix: add `Cors(origin)`

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
   const port = 3000;
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api', { exclude: ['health'] });
+  app.enableCors({ origin: ['http://localhost:3000'], credentials: true, exposedHeaders: ['Authorization'] });
 
   await app.listen(port);
   console.log(`Server is running on ${port}`);
