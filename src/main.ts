44aff2cb3e262ffc251d894779bd0f8a9870d17b
waveinyu <waveinyu@gmail.com>
2023-10-30 18:04:33 +0900

    Revert: "Fix: add `Cors(origin)`"
    This reverts commit b259d238e7a7c338d10d39cb388250cbbfe84dd0.

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ async function bootstrap() {
   const port = 3000;
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api', { exclude: ['health'] });
-  app.enableCors({ origin: ['http://localhost:3000'], credentials: true, exposedHeaders: ['Authorization'] });
 
   await app.listen(port);
   console.log(`Server is running on ${port}`);
