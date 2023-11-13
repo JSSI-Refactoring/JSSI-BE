80cae8d6f6d05b9cd0db177839dacba4551baac6
waveinyu <waveinyu@gmail.com>
2023-11-13 19:48:11 +0900

    Refactor: `kakaoLogin` 레이어 분리

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -5,6 +5,14 @@ import { Response } from 'express';
 import { CommonResponseDto } from 'src/common/dto/reponse.dto';
 import { KakaoLoginResponseDto } from './dto/response/kakao-login.dto';
 
+/** https://bobbyhadz.com/blog/typescript-property-does-not-exist-on-type */
+type KakaoLogin = {
+  data?: {
+    refreshToken: string;
+    accessToken: string;
+  };
+};
+
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService, private readonly configService: ConfigService) {}
@@ -12,21 +20,35 @@ export class AuthController {
   apiKey = this.configService.get('KAKAO_REST_API_KEY');
   redirectUri = this.configService.get('KAKAO_REDIRECT_URI');
 
+  @Get('/kakao')
+  async getAuthrization(@Res() res) {
+    const url = `https://kauth.kakao.com/oauth/authorize?client_id=${this.apiKey}&redirect_uri=${this.redirectUri}&response_type=code`;
+    return res.redirect(302, url); // 1-1. 인가코드 발급을 위해 redirect_uri로 리다이렉트
+  }
+
   // 프론트에서 발급 받은 인가코드로 카카오에 토큰 발급 요청
-  @Post('/kakaoLogin')
-  async kakaoLogin(@Query() query): Promise<CommonResponseDto<KakaoLoginResponseDto>> {
+  @Get('/kakaoLogin')
+  // @Post('/kakaoLogin')
+  async kakaoLogin(
+    @Query() query,
+    @Res({ passthrough: true }) res: Response,
+  ): Promise<CommonResponseDto<KakaoLoginResponseDto>> {
     try {
       const { code } = query;
       const apiKey = this.apiKey;
       const redirectUri = this.redirectUri;
 
-      // 카카오에 토큰 발급 요청
-      const myToken = await this.authService.requestTokensToKakao(apiKey, redirectUri, code);
-      const { token } = myToken;
+      const kakaoLogin: KakaoLogin = await this.authService.kakaoLogin(apiKey, redirectUri, code);
 
       // 응답 형식에 맞게 지정
       const kakaoResponse: KakaoLoginResponseDto = new KakaoLoginResponseDto();
-      kakaoResponse.token = token;
+      kakaoResponse.accessToken = kakaoLogin.data.accessToken;
+
+      res.setHeader('Authorization', `Bearer ${kakaoLogin.data.refreshToken}`);
+      res.cookie('refresh_token', kakaoLogin.data.refreshToken, {
+        httpOnly: true,
+        // maxAge: 24 * 60 * 60 * 1000 쿠키 유효 기간(하루)
+      });
 
       return {
         status: true,
@@ -35,7 +57,7 @@ export class AuthController {
         result: kakaoResponse,
       };
     } catch (err) {
-      console.log(err);
+      // console.log(err);
       throw new InternalServerErrorException('Internal server error');
     }
   }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -3,11 +3,65 @@ import { AuthDAO } from './models/auth.dao';
 import { ConfigService } from '@nestjs/config';
 import * as querystring from 'querystring';
 import axios from 'axios';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly authDAO: AuthDAO, private readonly configService: ConfigService) {}
+  constructor(
+    private readonly authDAO: AuthDAO,
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {}
 
+  async kakaoLogin(apiKey, redirectUri, code) {
+    const requestTokensToKakao = await this.requestTokensToKakao(apiKey, redirectUri, code);
+    const kakaoTokens = requestTokensToKakao.data;
+
+    const requestUserInfoToKakao = await this.requestUserInfoToKakao(kakaoTokens);
+    const { isNewUser, userInfo } = requestUserInfoToKakao;
+
+    // 신규 회원일 때
+    // 회원 등록
+    let result = {};
+    if (isNewUser) {
+      const createUser = await this.authDAO.createUser(userInfo, kakaoTokens);
+
+      const accessPayload = {
+        id: createUser.data.id,
+        nickname: userInfo.properties.nickname,
+        profileImage: userInfo.properties.profile_image,
+      };
+
+      const hashIndex = await this.hashUserIdForRefreshToken(createUser.data.id);
+      const refreshPayload = {
+        hash: hashIndex,
+      };
+
+      const [refreshToken, accessToken] = [
+        this.generateRefreshToken(refreshPayload),
+        this.generateAccessToken(accessPayload),
+      ];
+
+      const updateAppTokenOfUser = await this.authDAO.updateAppTokensOfUser(
+        createUser.data.id,
+        refreshToken,
+        accessToken,
+      );
+
+      result = { data: { refreshToken, accessToken } };
+      return result;
+    } else {
+      console.log('here');
+    }
+
+    // 기존 회원일 때
+    // 액세스 토큰 유효기간 확인
+    // 유효기간 만료: 새로운 액세스 토큰 발급
+    // 유효기간 만료X: 해당 액세스 토큰 반환
+  }
+
+  /** 카카오 토큰 요청 */
   async requestTokensToKakao(apiKey, redirectUri, code) {
     const urlToRequest = 'https://kauth.kakao.com/oauth/token';
     const headers = {
@@ -21,17 +75,46 @@ export class AuthService {
       code,
     });
 
-    // 토큰 발급 요청
     const response = await axios.post(urlToRequest, body, { headers });
-    // 토큰 발급 성공
-    const tokens = { accessToken: response.data.access_token, refreshToken: response.data.refresh_token };
+    const kakaoTokens = { accessToken: response.data.access_token, refreshToken: response.data.refresh_token };
+    const result = { data: kakaoTokens };
 
-    // 발급받은 토큰으로 카카오 회원 정보 요청
+    return result;
+  }
+
+  /** 카카오 회원 정보 요청 */
+  async requestUserInfoToKakao(kakaoTokens) {
     const getUserInfo = await axios.get('https://kapi.kakao.com/v2/user/me', {
-      headers: { Authorization: `Bearer ${tokens.accessToken}` },
+      headers: { Authorization: `Bearer ${kakaoTokens.accessToken}` },
     });
-    const userInfo = getUserInfo.data;
+    const userInfo = getUserInfo.data; // 발급한 토큰 정보
+    const socialId = userInfo.id;
 
-    return await this.authDAO.kakaoSignIn(tokens, userInfo);
+    const isNewUser = await this.authDAO.selectOneUserBySocialId(socialId);
+    const result = { isNewUser, userInfo };
+
+    return result;
+  }
+
+  /** 리프레쉬 토큰 발급 */
+  private generateRefreshToken(payload) {
+    return this.jwtService.sign(payload, {
+      secret: this.configService.get('JWT_REFRESH_SECRET'),
+      expiresIn: this.configService.get('JWT_REFRESH_EXPIRATION_TIME'),
+    });
+  }
+
+  /** 액세스 토큰 발급 */
+  private generateAccessToken(payload) {
+    return this.jwtService.sign(payload, {
+      secret: this.configService.get('JWT_ACCESS_SECRET'),
+      expiresIn: this.configService.get('JWT_ACCESS_EXPIRATION_TIME'),
+    });
+  }
+
+  /** 리프레쉬 토큰 인덱스 암호화 */
+  private async hashUserIdForRefreshToken(id) {
+    const result = await bcrypt.hash(id.toString(), 10);
+    return result;
   }
 }
